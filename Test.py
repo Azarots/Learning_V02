cbb9c5abbf6d2388b4571afcbf689180e3ae3e1e
Azarots <Azarots159@gmail.com>
2023-03-19 16:24:41 +0100

    Practice

---
+++ Test.py
@@ -0,0 +1,22 @@
+# number = float(input("Enter your number: "))
+#
+# amount = 0
+#
+# while number >= 0:
+#     amount += number
+#     number = float(input("Enter your number: "))
+#
+# print(f"You entere a negative number {number}")
+# print(f"Summ amount {amount}")
+
+
+# my_list = []
+# number_wors = int(input("How many words: "))
+#
+# for sarasa in range(number_wors):
+#     my_list.append(input("Enter your word: "))
+#
+# print(f"Number of words {len(my_list)}")
+#
+# for word in my_list:
+#     print(f"Zodzio {word} ilgis {len(word)} ir eiles numeris {my_list.index(word)+1}")
