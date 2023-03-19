cdaaf34368a7b7f60bf789518d09a5e5fa8959b5
Azarots <Azarots159@gmail.com>
2023-03-19 15:38:12 +0100

    Comprehensions

---
+++ List Comprehensions.py
@@ -0,0 +1,31 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# my_list = []
+# for n in nums:
+#     my_list.append(n)
+# print(my_list)
+
+# my_list = [n for n in nums]
+# print(my_list)
+#
+# my_list = [n*n for n in nums]
+# print(my_list)
+
+# my_list = [n for n in nums if n%2 == 0]
+# print(my_list)
+#
+# my_list = filter(lambda n: n%2 == 0, nums)
+# print(list(my_list))
+
+# my_list = [(letter, num) for letter in "abcd" for num in range(4)]
+# print(my_list)
+
+names = ["Bruce", "Clark", "Peter", "Logan", "Wade"]
+heros = ["Batman", "Superman", "Spiderman", "Wolverine", "Deadpool"]
+
+# print(list(zip(names, heros)))
+
+# my_dict = {name: hero for name, hero in zip(names, heros) if name != "Peter"}
+# print(my_dict)
+#
+# my_set = {n for n in nums}
